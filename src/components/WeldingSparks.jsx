0eed3f41ae4dc31e88ab4bf36869e8c0fd9d26f6
heyiou <djcatcob@yahoo.com>
2026-01-14 18:27:25 -0600

    Make slag trail gray darker to [40,40,40]

--- src/components/WeldingSparks.jsx
+++ src/components/WeldingSparks.jsx
@@ -145,7 +145,7 @@ const WeldingSparks = () => {
           // Define color stops
           const white = [255, 255, 255];
           const orange = [255, 140, 0];
-          const darkGray = [70, 70, 70];
+          const darkGray = [40, 40, 40];
           
           let r, g, b;
           if (agePercent <= 0.1) {
