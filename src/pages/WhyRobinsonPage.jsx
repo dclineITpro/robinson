d65a343b6135e05fa7bbfb0cd56b3d9df25ac5f2
heyiou <djcatcob@yahoo.com>
2026-01-15 19:08:28 -0600

    remove unverified sqft from locations

--- src/pages/WhyRobinsonPage.jsx
+++ src/pages/WhyRobinsonPage.jsx
@@ -32,11 +32,11 @@ const WhyRobinsonPage = () => {
   ];
 
   const locations = [
-    { name: 'De Pere Main Campus', type: 'Headquarters', sqft: '185,000', address: '1740 Eisenhower Rd' },
-    { name: 'De Pere Facility 2', type: 'Manufacturing', sqft: '179,000', address: '2107 American Blvd' },
-    { name: 'De Pere Facility 3', type: 'Manufacturing', sqft: '275,000', address: '2121 American Blvd' },
-    { name: 'Manitowoc Campus', type: 'Manufacturing', sqft: '137,000', address: '2121 South 30th Street' },
-    { name: 'Altoona, IA (2027)', type: 'New Facility', sqft: '560,000', isNew: true, address: 'Coming Soon' },
+    { name: 'De Pere Main Campus', type: 'Headquarters', address: '1740 Eisenhower Rd' },
+    { name: 'De Pere Facility 2', type: 'Manufacturing', address: '2107 American Blvd' },
+    { name: 'De Pere Facility 3', type: 'Manufacturing', address: '2121 American Blvd' },
+    { name: 'Manitowoc Campus', type: 'Manufacturing', address: '2121 South 30th Street' },
+    { name: 'Altoona, IA (2027)', type: 'New Facility', isNew: true, address: 'Coming Soon' },
   ];
 
   const whyThisRole = [
@@ -279,10 +279,6 @@ const WhyRobinsonPage = () => {
                   <div className="flex items-center gap-2">
                     <MapPin className="w-4 h-4 text-robinson-blue" />
                     <span className="text-robinson-gray text-sm">{location.address}</span>
-                  </div>
-                  <div className="flex items-center gap-2">
-                    <Building2 className="w-4 h-4 text-robinson-blue" />
-                    <span className="text-robinson-gray text-sm">{location.sqft} sq. ft.</span>
                   </div>
                 </div>
               </motion.div>
