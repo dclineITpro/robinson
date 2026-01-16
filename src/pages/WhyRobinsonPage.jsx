203c53b4d5a7b302bc1399ce050391f6dd256d74
heyiou <djcatcob@yahoo.com>
2026-01-15 20:45:48 -0600

    change De Pere Main Campus to De Pere Facility 1

--- src/pages/WhyRobinsonPage.jsx
+++ src/pages/WhyRobinsonPage.jsx
@@ -32,7 +32,7 @@ const WhyRobinsonPage = () => {
   ];
 
   const locations = [
-    { name: 'De Pere Main Campus', type: 'Production Facility', sqft: '185,000', address: '1740 Eisenhower Rd' },
+    { name: 'De Pere Facility 1', type: 'Production Facility', sqft: '185,000', address: '1740 Eisenhower Rd' },
     { name: 'De Pere Facility 2', type: 'Production Facility', sqft: '180,000', address: '2107 American Blvd' },
     { name: 'De Pere Facility 3', type: 'Production Facility', sqft: '240,000', address: '2121 American Blvd' },
     { name: 'Manitowoc Campus', type: 'Production Facility', sqft: '137,000', address: '2121 South 30th Street' },
